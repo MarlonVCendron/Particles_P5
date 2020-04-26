fe8f69e14311b0e49182323f7ca1d8af78e3769d
marlonvcendron <marlonvcendron@gmail.com>
2020-04-26 17:07:37 -0300

    Fixing some things

--- particle.js
+++ particle.js
@@ -31,48 +31,73 @@ class Particle{
   }
 
   update(){
-    this.vel.mult(friction);
+    this.checkWall();
+    this.mouse();
 
     this.vel.add(this.acc);
     this.pos.add(this.vel);
 
-    this.hitTheWall();
-    this.mouse();
+    this.vel.mult(friction);
 
     this.acc = createVector();
+  }
+
+  checkWall(){
+    if(this.pos.x > width){
+      this.pos.x = width;
+    }else if(this.pos.x < 0){
+      this.pos.x = 0;
+    }
+    if(this.pos.y > height){
+      this.pos.y = height;
+    }else if(this.pos.y < 0){
+      this.pos.y = 0;
+    }
+  }
+
+  // interact(target, type){
+  //   let force = p5.Vector.sub(target, this.pos);
+  //   let dsquared = force.magSq();
+  //   dsquared = constrain(dsquared, 25, 1000);
+  //   let strength = 10 / dsquared;
+  //   force.setMag(strength);
+  //
+  //   if(this.pos.dist(target) < this.d*2){
+  //     force.mult(-1);
+  //   }
+  //
+  //   this.acc.add(force);
+  // }
+
+  interact(target, type){
+    let force = p5.Vector.sub(target, this.pos);
+    let dsquared = force.magSq();
+    dsquared = constrain(dsquared, 25, 1000);
+    let strength = 0;
+    // console.log(force.mag()/1000);
+
+    if(this.pos.dist(target) < this.d*4){
+      strength = -10 / dsquared;
+    }else{
+      if (force.mag() < 800) {
+        strength = Math.abs(-0.1 * Math.abs(force.mag()/1000));
+      }
+    }
+
+    force.setMag(strength);
+    this.acc.add(force);
   }
 
   mouse(){
     let mousePos = createVector(mouseX, mouseY);
     let force = p5.Vector.sub(mousePos, this.pos);
     let dsquared = force.magSq();
-    // dsquared = constrain(dsquared, 25, 1000);
-    let strength = 100 / dsquared;
+    dsquared = constrain(dsquared, 1000, 10000);
+    // let strength = 10000 / dsquared;
+    let strength = 1000 / force.mag();
+    strength = constrain(strength, 0, 10);
     force.setMag(strength);
-    // force.mult(-1);
+    force.mult(-1);
     this.acc.add(force);
-  }
-
-  hitTheWall(){
-    if(this.pos.x > width || this.pos.x < 0){
-      this.vel.x *= -1;
-    }else if(this.pos.y > height || this.pos.y < 0){
-      this.vel.y *= -1;
-    }
-  }
-
-  attract(target){
-    let force = p5.Vector.sub(target, this.pos);
-    let dsquared = force.magSq();
-    dsquared = constrain(dsquared, 25, 1000);
-    let strength = 10 / dsquared;
-    force.setMag(strength);
-    if(this.pos.dist(target) > this.d*2){
-      this.acc.add(force);
-    }else{
-      force.mult(-1);
-      this.acc.add(force);
-    }
-
   }
 }
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+const numParticles = 250;
+const friction = 0.001;
+
 let particles = [];
-var friction = 0.8;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
 
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < numParticles; i++) {
     rX = random(width);
     rY = random(height);
     rType = floor(random(5));
@@ -17,12 +19,14 @@ function draw(){
   background(0);
 
   for(let p1 of particles){
+    p1.show();
+    p1.update();
     for(let p2 of particles){
-      p1.show();
-      p1.update();
       if(p1 != p2){
-        p1.attract(p2.pos);
+        p1.interact(p2.pos, p2.type);
       }
     }
   }
+
+  // console.log(frameRate());
 }
